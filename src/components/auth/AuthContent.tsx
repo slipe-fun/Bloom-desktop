5b8a6746ddab46ca9131aafa134513f6db40503b
GrenkaUkraine laptop <grenkaukrainefromukraine@gmail.com>
2026-03-01 00:35:03 +0200

    Added back button

--- src/components/auth/AuthContent.tsx
+++ src/components/auth/AuthContent.tsx
@@ -1,9 +1,18 @@
-import AuthSignIn from "./contents/AuthSignIn.tsx";
+import Button from "../ui/Button.tsx";
+import Icon from "../ui/Icon.tsx";
+import AuthSignUp from "./contents/AuthSignUp.tsx";
 
 export function AuthContent() {
   return (
-    // <AuthEmail/>
-    // <AuthVerify/>
-    <AuthSignIn/>
+    <>
+      {/*<AuthEmail/>*/}
+      {/*<AuthVerify/>*/}
+      <AuthSignUp/>
+      <div className="flex justify-center items-center w-1/3 fixed top-0 left-0 h-screen">
+        <Button variant="icon-secondary">
+          <Icon size={30} icon="chevron.left"/>
+        </Button>
+      </div>
+    </>
   )
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,22 +1,36 @@
 import {ButtonHTMLAttributes, forwardRef} from 'react';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary';
+  variant?: 'primary' | 'icon-secondary';
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({children, className = '', ...props}, ref) => {
+  ({children, variant = 'primary', className = '', ...props}, ref) => {
+    const baseStyles = `
+      h-super flex items-center justify-center rounded-full transition-all
+      active:scale-95 select-none
+      enabled:cursor-pointer disabled:cursor-not-allowed disabled:opacity-20
+      focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background
+    `;
+
+    const variants = {
+      primary: `
+        w-full bg-primary text-lg font-semibold text-white
+        enabled:hover:bg-backdrop-primary focus-visible:ring-primary/50
+      `,
+      'icon-secondary': `
+        aspect-square bg-background text-text-content shadow-md
+        enabled:hover:bg-foreground-soft focus-visible:ring-foreground
+      `,
+    };
+
     return (
       <button
         ref={ref}
         {...props}
         className={`
-          flex h-super w-full items-center justify-center
-          rounded-full bg-primary text-lg font-semibold text-white transition-all
-          active:scale-95 select-none
-          enabled:cursor-pointer enabled:hover:bg-backdrop-primary
-          disabled:cursor-not-allowed disabled:opacity-20
-          focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary/50 focus-visible:ring-offset-2 focus-visible:ring-offset-background
+          ${baseStyles}
+          ${variants[variant]}
           ${className}
         `}
       >
