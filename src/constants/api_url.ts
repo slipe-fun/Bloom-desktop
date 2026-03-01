af844ca2a5f044303c577d6b99eecba97ec7fca2
Aqui <aquiren@proton.mail>
2026-03-01 16:05:15 +0500

    Replaced localhost API_URL with the real API_URL, CORS not fixed

--- src/constants/api_url.ts
+++ src/constants/api_url.ts
@@ -1,4 +1,4 @@
-// export const API_URL = 'https://api.bloomapp.pw' as const
-export const API_URL = 'http://127.0.0.1:8080' as const
+// export const API_URL = 'http://127.0.0.1:8080' as const
+export const API_URL = 'https://api.bloomapp.pw' as const
 export const WEBSOCKET_URL = 'wss://api.bloomapp.pw/ws?token=' as const
 export const CDN_URL = 'https://cdn.bloomapp.pw' as const
