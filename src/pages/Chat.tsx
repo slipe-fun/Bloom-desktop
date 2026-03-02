e8945cb5d43b2e4e503d1fd283806bea00e70e41
Aqui <aquiren@proton.mail>
2026-03-02 11:10:26 +0500

    Add the chat page and a TextInput with a send button

---
+++ src/pages/Chat.tsx
@@ -0,0 +1,11 @@
+import { Chat } from "../components/app/Chat.tsx";
+// import ChatList from "../components/app/ChatList.tsx";
+
+export function ChatRoute()
+{
+    return (
+        <main>
+            <Chat/>
+        </main>
+    );
+}
