b925082dd5d670f33b41a2100c4405441499ea0e
GrenkaUkraine laptop <grenkaukrainefromukraine@gmail.com>
2026-03-04 14:31:09 +0200

    fix icon color

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -17,7 +17,7 @@ export function Chat() {
                 className="flex flex-col w-full rounded-xxxl bg-foreground-soft p-xxl gap-lg justify-center items-center"
               >
                 <span className="flex size-xxsuper justify-center items-center bg-foreground-transparent rounded-full">
-                  <Icon size={44} icon="message"/>
+                  <Icon className="text-text-main border border-text-main" size={44} icon="message"/>
                 </span>
                 <h1 className="text-md text-text-main text-center w-full">
                   Your chat list is empty, let’s create a new chat!
