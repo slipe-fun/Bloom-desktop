89d070b4f7c86e0d4666ba64eaeedd732b3f5d70
Dikiy Perec <hopabsq@mail.ru>
2026-07-26 23:50:03 +0600

    Add avatar to success

--- src/components/auth/success-method.tsx
+++ src/components/auth/success-method.tsx
@@ -1,12 +1,55 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import { motion } from "framer-motion"
+import Flame from "@/assets/icons/flame.svg?react"
+import Lightbolt from "@/assets/icons/lightbolt.svg?react"
+import { EASING } from "@/constants/animations-easing"
+
+const MotionFlame = motion(Flame)
+const MotionLightbolt = motion(Lightbolt)
 
 export function AuthSuccessMethod() {
   return (
     <>
-      <Avatar size="lg">
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+      <div className="relative flex size-35">
+        <MotionFlame
+          initial={{
+            opacity: 0,
+            scale: 0.5,
+            filter: "blur(2px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(1px)",
+          }}
+          transition={EASING.springyTimed}
+          className="absolute bottom-0 h-22 w-22 -translate-x-7.5 translate-y-3.5 -rotate-19 text-orange-500"
+        />
+        <MotionLightbolt
+          initial={{
+            opacity: 0,
+            scale: 0.5,
+            filter: "blur(4px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(2px)",
+          }}
+          transition={{
+            ...EASING.springyTimed,
+            delay: 0.05,
+          }}
+          className="absolute top-0 right-0 h-20.5 w-20.5 translate-x-3 -translate-y-6.25 rotate-19 text-blue-500"
+        />
+        <Avatar className="size-35">
+          <AvatarImage
+            src="https://i.pinimg.com/736x/98/26/5b/98265b01771083f8c6e6472fc170a682.jpg"
+            alt="Dikiy Dikiens"
+          />
+          <AvatarFallback className="text-4xl">DD</AvatarFallback>
+        </Avatar>
+      </div>
 
       <p className="text-2xl font-bold text-foreground/40">
         Welcome <span className="text-foreground">Dikiy Dikiens</span>
